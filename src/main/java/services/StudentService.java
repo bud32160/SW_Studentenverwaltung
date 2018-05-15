d0a527641a331123136bfedf88d44d88dd399bfa
David Burger <bud32160@gmail.com>
2018-05-15 11:45:58 +0200

    Added web formulars to create objects

--- src/main/java/services/StudentService.java
+++ src/main/java/services/StudentService.java
@@ -1,5 +1,23 @@
 package services;
 
+import entities.Address;
+import entities.Student;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@RequestScoped
 public class StudentService {
+    
+    @PersistenceContext(unitName="StudentenverwaltungPU")
+    private EntityManager em;
+    
+    @Transactional
+    public void createStudent(Address address, Student student){
+        em.persist(address);
+        em.persist(student);
+    }
+    
     
 }
