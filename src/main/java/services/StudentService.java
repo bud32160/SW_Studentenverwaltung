7d174487633d801162ef591b760c8c89fa27a427
David Burger <bud32160@gmail.com>
2018-05-16 13:05:02 +0200

    Created first query - getStudent()

--- src/main/java/services/StudentService.java
+++ src/main/java/services/StudentService.java
@@ -1,8 +1,11 @@
 package services;
 
+import entities.Student;
+import entities.User;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @RequestScoped
 public class StudentService {
@@ -10,6 +13,27 @@ public class StudentService {
     @PersistenceContext(unitName="StudentenverwaltungPU")
     private EntityManager em;
     
+    public Student getStudent(User user){
+        User studentUser;
+        
+        // search for studentobject
+        String selectCommand = "SELECT u from User AS u WHERE mailAddress = " + user.getMailAddress();
+        TypedQuery<User> query = em.createQuery(selectCommand, User.class);
+        studentUser = query.getSingleResult();
+        
+        // check if password is correct
+        if(user.getPassword().equals(studentUser.getPassword()))
+        {
+            String s = "SELECT s FROM STUDENT AS s WHERE mailAddress = " + user.getMailAddress();
+            // return the student object
+            TypedQuery<Student> q = em.createQuery(s, Student.class);
+            
+            return q.getSingleResult();
+        }
+        else{
+            return null;
+        }
+    }
     
     
     
